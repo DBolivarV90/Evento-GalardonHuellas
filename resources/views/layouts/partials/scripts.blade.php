6a8128c506ed492af46d64dd5073fd8e3f717a3b
Diego Bolivar <dbolivarv90@gmail.com>
2021-07-28 14:15:53 -0500

    Actualizacion Script

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -5,14 +5,7 @@
 
     <script>
         // Your web app's Firebase configuration
-        var firebaseConfig = {
-          apiKey: "AIzaSyDlCBl1v002c_sWSZYqVkofR9bEwUjD90I",
-          authDomain: "chatprueba-beb34.firebaseapp.com",
-          projectId: "chatprueba-beb34",
-          storageBucket: "chatprueba-beb34.appspot.com",
-          messagingSenderId: "136238188018",
-          appId: "1:136238188018:web:ee3f3086505100fb50176b"
-        };
+        
         // Initialize Firebase
         firebase.initializeApp(firebaseConfig);
     </script>
